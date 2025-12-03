5e650fcdf45ad4f05b3813cbd45af427a9964fa4
Alejandro MR <alejandromr@MacBook-Air-de-Alejandro.local>
2025-12-02 20:32:36 -0500

    09-modules

--- 10-typescript/dist/09.modules.js
+++ 10-typescript/dist/09.modules.js
@@ -1,2 +1,10 @@
 import { sumar } from './math.js';
-console.log(sumar(3, 4));
+const output9 = document.getElementById('output9');
+let nmb1 = Number(prompt('Lets perform an addition, enter the first number: '));
+let nmb2 = Number(prompt('Enter the second number: '));
+let result = sumar(nmb1, nmb2);
+if (output9) {
+    output9.innerHTML = `<li>Number 1: ${nmb1}</li>
+                        <li>Number 2: ${nmb2}</li>
+                        <li>Result: ${result}</li>`;
+}
--- 10-typescript/src/09.modules.ts
+++ 10-typescript/src/09.modules.ts
@@ -1,3 +1,14 @@
 import {sumar} from './math.js';
 
-console.log(sumar(3,4));
+const output9 = document.getElementById('output9');
+
+
+let nmb1 : number = Number(prompt('Lets perform an addition, enter the first number: '));
+
+let nmb2 : number = Number(prompt('Enter the second number: '));
+let result : number = sumar(nmb1, nmb2);
+if(output9){
+    output9.innerHTML = `<li>Number 1: ${nmb1}</li>
+                        <li>Number 2: ${nmb2}</li>
+                        <li>Result: ${result}</li>`
+}
