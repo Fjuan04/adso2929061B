50ee8a7734129222c675f0d2cd715f48fcb8aa60
fjuan04 <juanda.juanes@gmail.com>
2025-12-04 22:53:25 -0500

    typescript type assertion / type predicated

--- 10-typescript/dist/11-type-guards.js
+++ 10-typescript/dist/11-type-guards.js
@@ -26,16 +26,42 @@ class Bird extends Animal {
 }
 let perro = new Perro;
 let bird = new Bird;
+function isPlay(c) {
+    return c.suscription == 'Ps Plus';
+}
+let xbox = {
+    powered: 'Microsoft',
+    latest: 'Series X',
+    suscription: 'GamePass'
+};
+let ps5 = {
+    powered: 'Sony',
+    'latest': 'Ps5',
+    'suscription': 'Ps Plus'
+};
+function render(console) {
+    if (isPlay(console)) {
+        if (output11) {
+            for (let k in console) {
+                const key = k;
+                output11.innerHTML += `<li class="w-full border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${key}</span> ${console[key]}</li>`;
+            }
+        }
+    }
+}
 if (output11) {
-    output11.innerHTML = `
-    <h3>typeof Guard</h3>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show('Hornet')}</span> ${show('Hornet')}</li>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show(90)}</span> Has alcanzado el ${show(90)}% del juego</li>
-
-    <h3>typeof Guard</h3>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Perro</span> Este animal: ${perro.move(perro)}</li>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Pajaro</span> Este animal: ${bird.move(bird)}</li>
-
-    <h3>type predicated Functions</h3>
-    `;
+    output11.innerHTML += `
+        <h3 class='mb-2'>typeof Guard</h3>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show('Hornet')}</span> ${show('Hornet')}</li>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show(90)}</span> Has alcanzado el ${show(90)}% del juego</li>
+        
+        <h3 class='mb-2'>instanceof Guard</h3>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Perro</span> Este animal: ${perro.move(perro)}</li>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Pajaro</span> Este animal: ${bird.move(bird)}</li>
+        
+        <h3 class='mb-2'>type predicated / type assertion</h3>
+        
+        
+        `;
+    render(ps5);
 }
--- 10-typescript/src/11-type-guards.ts
+++ 10-typescript/src/11-type-guards.ts
@@ -29,22 +29,65 @@ class Animal {
 class Perro extends Animal {}
 class Bird extends Animal {}
 
-let perro : Perro = new Perro;
-let bird : Bird = new Bird;
+let perro = new Perro; 
+let bird  = new Bird;
 
 
+//Type predicated functions
+interface Xbox {
+    powered : string
+    latest : string
+    suscription : 'GamePass'
 
-
-if(output11){
-    output11.innerHTML = `
-    <h3>typeof Guard</h3>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show('Hornet')}</span> ${show('Hornet')}</li>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show(90)}</span> Has alcanzado el ${show(90)}% del juego</li>
-
-    <h3>typeof Guard</h3>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Perro</span> Este animal: ${perro.move(perro)}</li>
-    <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Pajaro</span> Este animal: ${bird.move(bird)}</li>
-
-    <h3>type predicated Functions</h3>
-    `;
 }
+
+interface Play {
+    powered : string
+    latest : string
+    suscription : 'Ps Plus'
+}
+
+function isPlay(c : Xbox | Play): c is Play {
+    return (c as Play).suscription == 'Ps Plus';
+} 
+
+let xbox : Xbox = {
+    powered : 'Microsoft',
+    latest : 'Series X',
+    suscription : 'GamePass'
+}
+
+let ps5 : Play = {
+    powered : 'Sony',
+    'latest' : 'Ps5',
+    'suscription' : 'Ps Plus'
+};
+
+function render(console : Play | Xbox):void {
+    if(isPlay(console)){
+        if(output11){
+                for(let k in console){
+                    const key = k as keyof typeof console;
+                    output11.innerHTML += `<li class="w-full border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${key}</span> ${console[key]}</li>`;
+                }
+            }
+        }
+    }
+    
+    if(output11){
+        output11.innerHTML += `
+        <h3 class='mb-2'>typeof Guard</h3>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show('Hornet')}</span> ${show('Hornet')}</li>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>${typeof show(90)}</span> Has alcanzado el ${show(90)}% del juego</li>
+        
+        <h3 class='mb-2'>instanceof Guard</h3>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Perro</span> Este animal: ${perro.move(perro)}</li>
+        <li class="border m-auto p-5 rounded-sm text-xl"><span class='badge badge-primary'>Pajaro</span> Este animal: ${bird.move(bird)}</li>
+        
+        <h3 class='mb-2'>type predicated / type assertion</h3>
+        
+        
+        `;
+        render(ps5);
+    }
+
